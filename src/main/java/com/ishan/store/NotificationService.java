241ff16d6b3de4df5556d7a49dd77585692d59db
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-08-07 14:14:57 +0545

    implement user registration service

--- src/main/java/com/ishan/store/NotificationService.java
+++ src/main/java/com/ishan/store/NotificationService.java
@@ -1,5 +1,5 @@
 package com.ishan.store;
 
 public interface NotificationService {
-    void send(String message);
+    void send(String message, String recipientEmail);
 }
--- src/main/java/com/ishan/store/StoreApplication.java
+++ src/main/java/com/ishan/store/StoreApplication.java
@@ -9,7 +9,12 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var resource = context.getBean(HeavyResource.class);
+
+		var userService = context.getBean(UserService.class);
+		userService.registerUser(new User(1L, "mosh@codewithmosh.com", "12345", "Mosh"));
+		userService.registerUser(new User(1L, "albert@codewithalbert.com", "12348", "Albert"));
+//		var resource = context.getBean(HeavyResource.class);
+
 //		var orderService = context.getBean(OrderService.class);
 //		orderService.placeOrder();
 //		var notificationService = context.getBean(NotificationService.class);
