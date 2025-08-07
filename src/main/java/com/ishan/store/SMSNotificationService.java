d84fa980803e71d394466750a85ab50c5ab788b1
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-08-07 13:41:35 +0545

    bean configurations using code

--- src/main/java/com/ishan/store/SMSNotificationService.java
+++ src/main/java/com/ishan/store/SMSNotificationService.java
@@ -3,9 +3,10 @@ package com.ishan.store;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SMSNotificationSerivce implements NotificationService{
+public class SMSNotificationService implements NotificationService{
     @Override
     public void send(String message) {
         System.out.println("Sending SMS: " + message);
     }
 }
+
